15486091da318571d9e35213fc78b8c61292071d
Raffaele <raffaeleturra@gmail.com>
2021-04-07 23:29:07 +0200

    feat: implementa il file di config dev

--- jekyll-serve.bat
+++ jekyll-serve.bat
@@ -1,1 +1,5 @@
-docker run --rm  --volume="C:\Users\raffaele.turra\OneDrive\projects\seelake\seelake.github.io:/srv/jekyll" -p 4000:4000 -it jekyll/jekyll  jekyll serve
+REM test locally
+docker run --rm  --volume="C:\Users\raffaele.turra\OneDrive\projects\seelake\seelake.github.io:/srv/jekyll" -p 4000:4000 -it jekyll/jekyll  jekyll serve --config _config.yml,_config_dev.yml --watch
+
+REM build online
+docker run --rm  --volume="C:\Users\raffaele.turra\OneDrive\projects\seelake\seelake.github.io:/srv/jekyll" -it jekyll/jekyll jekyll build --config _config.yml --watch
